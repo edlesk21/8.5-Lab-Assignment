cfb43163a9ca32bd51431921871ab72d8bd57acd
EJ Leskauskas <leskaus1@msu.edu>
2024-03-18 19:22:32 -0400

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Add JavaScript below
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ptagzouxflldprbgmtrg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0YWd6b3V4ZmxsZHByYmdtdHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MDEyNjIsImV4cCI6MjAyNjM3NzI2Mn0.mj6es7_vDdoAl7GWkiGrLJBmqtNRJiEmxam4-5WTKlo'
+const supabase = createClient(supabaseUrl, supabaseKey)
