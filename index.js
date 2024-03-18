10a0cf6516f9fce575e6c3c1c61783b2642d78c8
Ej Leskauskas <143560945+edlesk21@users.noreply.github.com>
2024-03-18 23:48:34 +0000

    Changes

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ptagzouxflldprbgmtrg.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0YWd6b3V4ZmxsZHByYmdtdHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MDEyNjIsImV4cCI6MjAyNjM3NzI2Mn0.mj6es7_vDdoAl7GWkiGrLJBmqtNRJiEmxam4-5WTKlo'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks(){
+    let { data: books, error } = await supabase
+    .from('books')
+    .select('*')
+
+    for (let book of books) {
+        let bookList = document.getElementById('books');
+        bookList.innerHTML += '<li>$(book.title) - $(book.author) - $(book.ISBN)</li>';
+      }
+}
+
+  getBooks();
