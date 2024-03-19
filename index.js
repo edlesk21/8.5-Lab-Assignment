82b4babcc73d08859880a0a90662f3d298835bb3
Ej Leskauskas <143560945+edlesk21@users.noreply.github.com>
2024-03-19 00:04:20 +0000

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add JavaScript below
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.7.1"
 
 const supabaseUrl = 'https://ptagzouxflldprbgmtrg.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0YWd6b3V4ZmxsZHByYmdtdHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MDEyNjIsImV4cCI6MjAyNjM3NzI2Mn0.mj6es7_vDdoAl7GWkiGrLJBmqtNRJiEmxam4-5WTKlo'
